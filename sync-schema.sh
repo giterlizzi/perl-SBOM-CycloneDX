8f44ac5e5e17223ef48606fce1b4e993812fcb2d
Giuseppe Di Terlizzi <giuseppe.diterlizzi@gmail.com>
2026-01-21 00:43:20 +0100

    Updated sync & fetch scripts

--- sync-schema.sh
+++ sync-schema.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-SPDX_LICENSES_SCHEMA_URL=https://raw.githubusercontent.com/CycloneDX/specification/refs/heads/master/schema/spdx.schema.json
-
 cd $(dirname $0) ; CWD=$(pwd)
+
+SPDX_LICENSES_SCHEMA_URL=https://raw.githubusercontent.com/CycloneDX/specification/refs/heads/master/schema/spdx.schema.json
 
 curl -o $CWD/lib/SBOM/CycloneDX/schema/spdx.schema.json $SPDX_LICENSES_SCHEMA_URL
